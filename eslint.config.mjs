2b79702bb12c6a4f174cf0eb557b1aa4a1252bb6
Khang Nguyen <44516773+ngkhang@users.noreply.github.com>
2025-02-16 21:14:35 +0700

    Setup/initial Implement code quality tool, git hook and commit style (#2)
    - Code quality tools: ESLint, Prettier
    - Git workflow tools: Husky, lint-staged, Commitlint, and Commitizen
    - Add environment variable and validation with @t3-oss/env-nextjs and Zod

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -10,7 +11,83 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: [
+      "next/core-web-vitals",
+      "next/typescript",
+      "prettier",
+      "plugin:import/recommended"
+    ],
+    "plugins": ["check-file", "n"],
+    "rules": {
+      // --- eslint rules ---
+      "arrow-body-style": ["error", "as-needed", { "requireReturnForObjectLiteral": true }],
+      "semi": "error",
+      "no-console": "error",
+      "no-nested-ternary": "error",
+      "no-lonely-if": "warn",
+      "no-undefined": "warn",
+      "no-unexpected-multiline": "warn",
+      "prefer-const": "error",
+      "prefer-arrow-callback": "warn",
+      // --- eslint-plugin-import rules ---
+      "import/export": "error",
+      "import/order": [
+        "error",
+        {
+          "groups": [
+            "builtin",
+            "external",
+            "internal",
+            ["parent", "sibling"],
+            "index",
+            "object"
+          ],
+          "newlines-between": "always",
+          "pathGroups": [
+            {
+              "pattern": "react",
+              "group": "builtin",
+              "position": "before"
+            },
+            {
+              "pattern": "next/**",
+              "group": "builtin",
+              "position": "before"
+            },
+            {
+              "pattern": "@/**",
+              "group": "internal",
+              "position": "after"
+            }
+          ],
+          "pathGroupsExcludedImportTypes": ["builtin"],
+          "alphabetize": {
+            "order": "asc",
+            "caseInsensitive": true
+          }
+        }
+      ],
+      // --- eslint-plugin-check-file rules ---
+      "check-file/filename-naming-convention": [
+        "error",
+        {
+          "**/*.{ts,tsx}": "KEBAB_CASE"
+        },
+        {
+          "ignoreMiddleExtensions": true
+        }
+      ],
+      "check-file/folder-naming-convention": [
+        "error",
+        {
+          "src/**/!^[.*": "KEBAB_CASE"
+        }
+      ],
+      // --- eslint-plugin-n rules---
+      "no-process-env": "error",
+    },
+  })
 ];
 
 export default eslintConfig;
